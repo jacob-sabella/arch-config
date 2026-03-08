e20dc7e1a2a1b9cb527f92aaa1158142e0b959db
Jacob Sabella <jacobsabella@outlook.com>
2026-03-08 15:07:30 -0500

    Migrate from symlinks to yadm: move dotfiles to real XDG paths
    dotfiles/* -> ~/.config/*
    dotfiles/bin/* -> ~/.local/bin/
    dotfiles/rofi-themes/* -> ~/.local/share/rofi/themes/
    assets/* -> ~/.local/share/arch-assets/

---
+++ .config/nvim/lua/plugins/cpp.lua
@@ -0,0 +1,26 @@
+return {
+  -- Tune clangd (extra imported in lazy.lua)
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-flags=-std=c++17",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+          },
+        },
+      },
+    },
+  },
+}
